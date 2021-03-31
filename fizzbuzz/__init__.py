6ddff2e2cbb148c3f96a4b2a48a55a5c88b18067
jorgenhlehne <32197885+jorgenhlehne@users.noreply.github.com>
2021-03-31 22:40:02 +0200

    fixing malformed db url

--- fizzbuzz/__init__.py
+++ fizzbuzz/__init__.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-heroku_db_url = 'postgres://psgsaifrjucpwf:da1deb4a94e5116f389bcae080c882f485482f3bf4a8b8146bc9e5a983c64a45@ec2-54-247-158-179.eu-west-1.compute.amazonaws.com:5432/dd3f9vjt6bvmvb'  # noqa
+heroku_db_url = 'postgresql://psgsaifrjucpwf:da1deb4a94e5116f389bcae080c882f485482f3bf4a8b8146bc9e5a983c64a45@ec2-54-247-158-179.eu-west-1.compute.amazonaws.com:5432/dd3f9vjt6bvmvb'  # noqa
 app.config['SQLALCHEMY_DATABASE_URI'] = heroku_db_url
 
 # Creates an SQLAlchemy database instance
